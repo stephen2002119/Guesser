3c92a0df82e34decea0ab1d601796815df36d25d
Stephen Dumore <stephen.dumore@liatrio.com>
2026-04-29 11:44:42 -0700

    feat: test cases for distance

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,40 @@ TEST(GuesserTest, smoke_test)
   Guesser object("Secret");
   ASSERT_EQ( 1+1, 2 );
 }
+
+TEST(GuesserTest, distance_gl_gt_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("Secrett"), 1);
+}
+TEST(GuesserTest, distance_gl_lt_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("Secre"), 1);
+}
+TEST(GuesserTest, distance_lb) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("secret"), 1);
+}
+TEST(GuesserTest, distance_ub) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("SecreT"), 1);
+}
+TEST(GuesserTest, distance_middle) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("SeCret"), 1);
+}
+TEST(GuesserTest, distance_lb_and_gl_lt_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("secre"), 2);
+}
+TEST(GuesserTest, distance_ld_eq_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("sECRET"), 6);
+}
+TEST(GuesserTest, distance_ld_one_gt_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("sECRETT"), 6);
+}
+TEST(GuesserTest, distance_ld_one_lt_sl) {
+  Guesser object("Secret");
+  ASSERT_EQ(object.distance("sECREt"), 5);
+}
